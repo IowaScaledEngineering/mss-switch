3afc8a859dae8726c2f40d556f618a2a4ef816fd
Nathan D. Holmes <ndholmes@fedex.com>
2024-10-11 18:34:53 -0600

    Pretty close to working actual firmware for a v1.0 board

--- src/mss-siding.c
+++ src/mss-siding.c
@@ -34,7 +34,29 @@ LICENSE:
 #include "signalHead.h"
 #include "mss.h"
 #include "i2c.h"
-#include "light_ws2812.h"
+
+
+#define LOOP_UPDATE_TIME_MS       50
+#define TURNOUT_LOCKOUT_TIME_MS  200
+#define STARTUP_LOCKOUT_TIME_MS  500
+
+#define TCA9555_ADDR_000  0x20
+#define TCA9555_GPIN0        0
+#define TCA9555_GPIN1        1
+#define TCA9555_GPOUT0       2
+#define TCA9555_GPOUT1       3
+#define TCA9555_GPDDR0       6
+#define TCA9555_GPDDR1       7
+
+// TCA9555 - GPIO 0
+//  IO07 - Output - Relay Driver (active high)
+//  IO06 - Output - Force Adv Approach on MSS Port A (points end)
+//  IO05 - Input  - Option Switch E
+//  IO04 - Input  - Option Switch D
+//  IO03 - Input  - Option Switch C
+//  IO02 - Input  - Option Switch B
+//  IO01 - Input  - Option Switch A
+//  IO00 - Input  - Signal Common Sense
 
 #define OPTION_COMMON_ANODE          0x01
 #define OPTION_A_APPROACH_LIGHTING   0x02
@@ -42,72 +64,28 @@ LICENSE:
 #define OPTION_C_SEARCHLIGHT_MODE    0x08
 #define OPTION_D_LIMIT_DIVERGING     0x10
 #define OPTION_E_RESERVED            0x20
+#define MSS_SET_FORCE_POINTS_AA      0x40
+#define MSS_SET_ROUTE_RELAY          0x80
 
 
-typedef enum
-{
-	STATUS_RED,
-	STATUS_YELLOW,
-	STATUS_GREEN,
-	STATUS_BLUE,
-	STATUS_PURPLE,
-	STATUS_WHITE,
-	STATUS_OFF,
-	STATUS_UNKNOWN,
-} Status;
+// TCA9555 - GPIO 1
+//  IO17 - Input  - Main AA In
+//  IO16 - Input  - Turnout Position (high = diverging)
+//  IO15 - Input  - Points S
+//  IO14 - Input  - Points A Out
+//  IO13 - Input  - Points A In
+//  IO12 - Input  - Points AA In
+//  IO11 - Input  - Points AA Out
+//  IO10 - Input  - Siding AA In
 
-
-void setStatusLED(Status status)
-{
-	static Status oldStatus = STATUS_UNKNOWN;
-	struct cRGB led;
-
-	if(status != oldStatus)
-	{
-		switch(status)
-		{
-			case STATUS_RED:
-				led.r = 64;
-				led.g = 0;
-				led.b = 0;
-				break;
-			case STATUS_YELLOW:
-				led.r = 64;
-				led.g = 24;
-				led.b = 0;
-				break;
-			case STATUS_GREEN:
-				led.r = 0;
-				led.g = 64;
-				led.b = 0;
-				break;
-			case STATUS_BLUE:
-				led.r = 0;
-				led.g = 0;
-				led.b = 64;
-				break;
-			case STATUS_PURPLE:
-				led.r = 64;
-				led.g = 0;
-				led.b = 64;
-				break;
-			case STATUS_WHITE:
-				led.r = 64;
-				led.g = 64;
-				led.b = 64;
-				break;
-			case STATUS_OFF:
-				led.r = 0;
-				led.g = 0;
-				led.b = 0;
-				break;
-			case STATUS_UNKNOWN:
-				break;
-		}
-		ws2812_setleds(&led,1);
-		oldStatus = status;
-	}
-}
+#define MSS_MAIN_AA_IN        0x80
+#define MSS_TO_IS_DIVERGING   0x40
+#define MSS_POINTS_S          0x20
+#define MSS_POINTS_A_OUT      0x10
+#define MSS_POINTS_A_IN       0x08
+#define MSS_POINTS_AA_IN      0x04
+#define MSS_POINTS_AA_OUT     0x02
+#define MSS_SIDING_AA_IN      0x01
 
 #define MIN(a,b) ((a)<(b)?(a):(b))
 #define MAX(a,b) ((a)>(b)?(a):(b))
@@ -118,6 +96,8 @@ SignalState_t signalAU;
 SignalState_t signalAL;
 SignalState_t signalB;
 SignalState_t signalC;
+
+void calculateAspects(uint8_t mssInputs, uint8_t optionJumpers);
 
 // A few hardware definitions
 
@@ -206,7 +186,7 @@ void initializeTimer()
 	// Set up Timer/Counter0 for 100Hz clock
 	TCCR0A = 0b00000001;  // CTC Mode
 	TCCR0B = 0b00000010;  // CS01 - 1:8 prescaler
-	OCR0A = 250;           // 8MHz / 8 / 125 = 8kHz
+	OCR0A = 250;           // 8MHz / 8 / 250 = 4kHz
 	TIMSK = _BV(OCIE0A);
 }
 
@@ -214,8 +194,11 @@ int main(void)
 {
 	DebounceState8_t optionsDebouncer;
 	DebounceState8_t mssDebouncer;
-	uint32_t lastReadTime = 0;
+	uint32_t lastReadTime = 0xf0000000;
 	uint32_t currentTime = 0;
+	uint8_t lastMSSInputs = 0;
+	uint8_t turnoutChangeLockout = (STARTUP_LOCKOUT_TIME_MS)/(LOOP_UPDATE_TIME_MS);
+	uint8_t initValue=0, i=0;
 	
 	// Deal with watchdog first thing
 	MCUSR = 0;              // Clear reset status
@@ -249,109 +232,77 @@ int main(void)
 	DDRB  = 0b01111111;
 
 	initializeTimer();
-	initDebounceState8(&optionsDebouncer, 0);
-	initDebounceState8(&mssDebouncer, 0);
 
 	signalHeadInitialize(&signalAU);
 	signalHeadInitialize(&signalAL);
 	signalHeadInitialize(&signalB);
 	signalHeadInitialize(&signalC);
 
-	signalHeadAspectSet(&signalAU, ASPECT_RED);
-	signalHeadAspectSet(&signalAL, ASPECT_RED);
-	signalHeadAspectSet(&signalB, ASPECT_RED);
-	signalHeadAspectSet(&signalC, ASPECT_RED);
+	signalHeadAspectSet(&signalAU, ASPECT_OFF);
+	signalHeadAspectSet(&signalAL, ASPECT_OFF);
+	signalHeadAspectSet(&signalB, ASPECT_OFF);
+	signalHeadAspectSet(&signalC, ASPECT_OFF);
 
-	setStatusLED(STATUS_OFF);
-
-	sei();
 	wdt_reset();
-
-#define TCA9555_ADDR_000  0x20
-#define TCA9555_GPIN0        0
-#define TCA9555_GPIN1        1
-#define TCA9555_GPOUT0       2
-#define TCA9555_GPOUT1       3
-#define TCA9555_GPDDR0       6
-#define TCA9555_GPDDR1       7
-
-#define MSS_MAIN_AA_IN        0x80
-#define MSS_TO_IS_DIVERGING   0x40
-#define MSS_POINTS_S          0x20
-#define MSS_POINTS_A_OUT      0x10
-#define MSS_POINTS_A_IN       0x08
-#define MSS_POINTS_AA_IN      0x04
-#define MSS_POINTS_AA_OUT     0x02
-#define MSS_SIDING_AA_IN      0x01
-
-#define OPTION_COMMON_ANODE          0x01
-#define OPTION_A_APPROACH_LIGHTING   0x02
-#define OPTION_B_FOUR_ASPECT         0x04
-#define OPTION_C_SEARCHLIGHT_MODE    0x08
-#define OPTION_D_LIMIT_DIVERGING     0x10
-#define OPTION_E_RESERVED            0x20
-#define MSS_SET_FORCE_POINTS_AA      0x40
-#define MSS_SET_ROUTE_RELAY          0x80
-
-
-	// TCA9555 - GPIO 0
-	//  IO07 - Output - Relay Driver (active high)
-	//  IO06 - Output - Force Adv Approach on MSS Port A (points end)
-	//  IO05 - Input  - Option Switch E
-	//  IO04 - Input  - Option Switch D
-	//  IO03 - Input  - Option Switch C
-	//  IO02 - Input  - Option Switch B
-	//  IO01 - Input  - Option Switch A
-	//  IO00 - Input  - Signal Common Sense
-
-	// TCA9555 - GPIO 1
-	//  IO17 - Input  - Main AA In
-	//  IO16 - Input  - Turnout Position (high = diverging)
-	//  IO15 - Input  - Points S
-	//  IO14 - Input  - Points A Out
-	//  IO13 - Input  - Points A In
-	//  IO12 - Input  - Points AA In
-	//  IO11 - Input  - Points AA Out
-	//  IO10 - Input  - Siding AA In
 
 	// Set TCA9555 direction registers
 	writeByte(TCA9555_ADDR_000, TCA9555_GPDDR0, 0b00111111);
 	writeByte(TCA9555_ADDR_000, TCA9555_GPDDR1, 0b11111111);
 	writeByte(TCA9555_ADDR_000, TCA9555_GPOUT0, 0b00000000);
-	
+
+	// Now that the IO expander and such as had a little time to settle
+	//  go get the initial values for options and MSS.  This should avoid
+	//  most of the initial bouncing around as the module starts up.
+
+	initValue = OPTION_COMMON_ANODE | OPTION_B_FOUR_ASPECT; // Default is common anode, four aspect
+	if (readByte(TCA9555_ADDR_000, TCA9555_GPIN0, &i))
+		initValue = i;
+	initDebounceState8(&optionsDebouncer, initValue);
+
+	initValue = 0; // Default - all lines clear, turnout normal
+	if (readByte(TCA9555_ADDR_000, TCA9555_GPIN1, &i))
+		initValue = i;
+	initDebounceState8(&mssDebouncer, initValue);
+
+	sei();
+
 	while(1)
 	{
 		wdt_reset();
 
 		currentTime = getMillis();
 
-		// Because debouncing and such is built into option reading and the MSS library, only 
-		//  run the updates every 10mS or so.
-
-		if (((uint32_t)currentTime - lastReadTime) > 50)
+		// We really don't need or want updates very often
+		//  50mS or so should be fine.  
+		if (((uint32_t)currentTime - lastReadTime) > LOOP_UPDATE_TIME_MS)
 		{
 			uint8_t optionJumpers = 0;
-			uint8_t ack = 0;
-			uint8_t i = 0;
-
-			SignalAspect_t aspectAU = ASPECT_RED;
-			SignalAspect_t aspectAL = ASPECT_RED;
-			SignalAspect_t aspectB = ASPECT_RED;
-			SignalAspect_t aspectC = ASPECT_RED;
-
+			uint8_t mssInputs = 0;
 			lastReadTime = currentTime;
 
 			// Read PCA9555
-			ack = readByte(TCA9555_ADDR_000, TCA9555_GPIN0, &i);
-			if (ack)
+			//  Note: readByte returns whether we got an ack
+			//  Only use the data on a valid read
+			if (readByte(TCA9555_ADDR_000, TCA9555_GPIN0, &i))
 				debounce8(i, &optionsDebouncer);
 
-			ack = readByte(TCA9555_ADDR_000, TCA9555_GPIN1, &i);
-			if (ack)
+			if (readByte(TCA9555_ADDR_000, TCA9555_GPIN1, &i))
 				debounce8(i, &mssDebouncer);
 
+			// Read state of MSS bus ports coming in
+			mssInputs = getDebouncedState(&mssDebouncer);
 			optionJumpers = getDebouncedState(&optionsDebouncer);
-			optionJumpers |= OPTION_B_FOUR_ASPECT | OPTION_C_SEARCHLIGHT_MODE | OPTION_D_LIMIT_DIVERGING | OPTION_A_APPROACH_LIGHTING;
+			// Options A-E are inverted (pulled up if open, low if soldered)
+			//  If they're selected, they're low.  Just invert them here so I
+			//  don't have to constantly think backwards
+			optionJumpers ^= 0b00111110;
+
+			if ((lastMSSInputs ^ mssInputs) & MSS_TO_IS_DIVERGING)
+				turnoutChangeLockout = (TURNOUT_LOCKOUT_TIME_MS) / (LOOP_UPDATE_TIME_MS);
+
+			lastMSSInputs = mssInputs;
+
+//			optionJumpers |= OPTION_B_FOUR_ASPECT | OPTION_C_SEARCHLIGHT_MODE | OPTION_D_LIMIT_DIVERGING | OPTION_A_APPROACH_LIGHTING;
 
 			// Convert global option bits to signal head option bits
 			if (optionJumpers & OPTION_C_SEARCHLIGHT_MODE)
@@ -364,77 +315,104 @@ int main(void)
 			else 
 				signalHeadOptions &= ~SIGNAL_OPTION_COMMON_ANODE;
 
-			// Read state of MSS bus ports coming in
-			i = getDebouncedState(&mssDebouncer);
 
+			// Calculate the output settings.  Basically, should we set the signal 
+			//  routing relay on (turnout to siding) or off (turnout to main)
+			// In addition, if we are thrown to the siding and Option D (limit diverging indication
+			//  to diverging approach) is set, figure out if we should send the 
+			//  advance approach indication down the line.  
 			{
 				uint8_t updateOutputs = 0;
-				if (i & MSS_TO_IS_DIVERGING)
+				if (mssInputs & MSS_TO_IS_DIVERGING)
 					updateOutputs |= MSS_SET_ROUTE_RELAY;
 
 				if ((optionJumpers & OPTION_D_LIMIT_DIVERGING)
-					&& (i & MSS_TO_IS_DIVERGING) && !(i & MSS_POINTS_A_OUT) )
+					&& (mssInputs & MSS_TO_IS_DIVERGING) && !(mssInputs & MSS_POINTS_A_OUT) )
 					updateOutputs |= MSS_SET_FORCE_POINTS_AA;
 					
 				writeByte(TCA9555_ADDR_000, TCA9555_GPOUT0, updateOutputs);
 			}
 
+			// If the turnout just changed, we need to give the relays time to settle down
+			//  and the new signal states to propagate through the debouncer.  Otherwise, calculate
+			//  the new aspects based on what came out of the debouncers
 
-			// Calculate signal aspects directly, since we don't have neatly-terminated
-			//  MSS busses but rather have to glean states from combinations of things.
-
-			if ((i & MSS_POINTS_S) || (i & MSS_TO_IS_DIVERGING))
-				aspectB = ASPECT_RED;
-			else if (i & MSS_POINTS_A_IN)
-				aspectB = ASPECT_YELLOW;
-			else if ((i & MSS_POINTS_AA_IN) && (optionJumpers & OPTION_B_FOUR_ASPECT))
-				aspectB = ASPECT_FL_YELLOW;
+			if (!turnoutChangeLockout)
+				calculateAspects(mssInputs, optionJumpers);
 			else
-				aspectB = ASPECT_GREEN;
-
-			if (i & MSS_POINTS_S || !(i & MSS_TO_IS_DIVERGING))
-				aspectC = ASPECT_RED;
-			else if (i & MSS_POINTS_A_IN)
-				aspectC = ASPECT_YELLOW;
-			else if ((i & MSS_POINTS_AA_IN) && (optionJumpers & OPTION_B_FOUR_ASPECT))
-				aspectC = ASPECT_FL_YELLOW;
-			else
-				aspectC = ASPECT_GREEN;
-
-			if (i & MSS_TO_IS_DIVERGING)
-			{
-				// Diverging point end signals
-				aspectAU = ASPECT_RED;
-			
-				if (i & MSS_POINTS_A_OUT)
-					aspectAL = ASPECT_RED;
-				else if ((i & MSS_POINTS_AA_OUT) || (optionJumpers & OPTION_D_LIMIT_DIVERGING))
-					aspectAL = ASPECT_YELLOW;
-				else if ((i & MSS_SIDING_AA_IN) && (optionJumpers & OPTION_B_FOUR_ASPECT))
-					aspectAL = ASPECT_FL_YELLOW;
-				else
-					aspectAL = ASPECT_GREEN;
-
-			} else {
-				aspectAL = ASPECT_RED;
-
-				if (i & MSS_POINTS_A_OUT)
-					aspectAU = ASPECT_RED;
-				else if ((i & MSS_POINTS_AA_OUT) && (optionJumpers & OPTION_B_FOUR_ASPECT))
-					aspectAU = ASPECT_YELLOW;
-				else if (i & MSS_MAIN_AA_IN)
-					aspectAU = ASPECT_FL_YELLOW;
-				else
-					aspectAU = ASPECT_GREEN;
-			}
-
-			signalHeadAspectSet(&signalAU, aspectAU);
-			signalHeadAspectSet(&signalAL, aspectAL);
-			signalHeadAspectSet(&signalB, aspectB);
-			signalHeadAspectSet(&signalC, aspectC);
+				turnoutChangeLockout--;
 		}
 	}
 }
+
+
+void calculateAspects(uint8_t mssInputs, uint8_t optionJumpers)
+{
+	SignalAspect_t aspectAU = ASPECT_RED;
+	SignalAspect_t aspectAL = ASPECT_RED;
+	SignalAspect_t aspectB = ASPECT_RED;
+	SignalAspect_t aspectC = ASPECT_RED;
+
+	// Calculate signal aspects directly, since we don't have neatly-terminated
+	//  MSS busses but rather have to glean states from combinations of things.
+
+	// SIGNAL A - Points End
+
+	if (mssInputs & MSS_TO_IS_DIVERGING)
+	{
+		// Diverging point end signals
+		aspectAU = ASPECT_RED;
+	
+		if (mssInputs & MSS_POINTS_A_OUT)
+			aspectAL = ASPECT_RED;
+		else if ((mssInputs & MSS_POINTS_AA_OUT) || (optionJumpers & OPTION_D_LIMIT_DIVERGING))
+			aspectAL = ASPECT_YELLOW;
+		else if ((mssInputs & MSS_SIDING_AA_IN) && (optionJumpers & OPTION_B_FOUR_ASPECT))
+			aspectAL = ASPECT_FL_YELLOW;
+		else
+			aspectAL = ASPECT_GREEN;
+
+	} else {
+		aspectAL = ASPECT_RED;
+
+		if (mssInputs & MSS_POINTS_A_OUT)
+			aspectAU = ASPECT_RED;
+		else if (mssInputs & MSS_POINTS_AA_OUT)
+			aspectAU = ASPECT_YELLOW;
+		else if ((mssInputs & MSS_MAIN_AA_IN ) && (optionJumpers & OPTION_B_FOUR_ASPECT))
+			aspectAU = ASPECT_FL_YELLOW;
+		else
+			aspectAU = ASPECT_GREEN;
+	}
+
+	// SIGNAL B - Frog End, Normal (Main)
+
+	if ((mssInputs & MSS_POINTS_S) || (mssInputs & MSS_TO_IS_DIVERGING))
+		aspectB = ASPECT_RED;
+	else if (mssInputs & MSS_POINTS_A_IN)
+		aspectB = ASPECT_YELLOW;
+	else if ((mssInputs & MSS_POINTS_AA_IN) && (optionJumpers & OPTION_B_FOUR_ASPECT))
+		aspectB = ASPECT_FL_YELLOW;
+	else
+		aspectB = ASPECT_GREEN;
+
+	// SIGNAL C - Frog End, Diverging/Reverse (Siding)
+
+	if (mssInputs & MSS_POINTS_S || !(mssInputs & MSS_TO_IS_DIVERGING))
+		aspectC = ASPECT_RED;
+	else if (mssInputs & MSS_POINTS_A_IN)
+		aspectC = ASPECT_YELLOW;
+	else if ((mssInputs & MSS_POINTS_AA_IN) && (optionJumpers & OPTION_B_FOUR_ASPECT))
+		aspectC = ASPECT_FL_YELLOW;
+	else
+		aspectC = ASPECT_GREEN;
+
+	signalHeadAspectSet(&signalAU, aspectAU);
+	signalHeadAspectSet(&signalAL, aspectAL);
+	signalHeadAspectSet(&signalB, aspectB);
+	signalHeadAspectSet(&signalC, aspectC);
+}
+
 
 
 
